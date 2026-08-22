14f6e034ff40627f1c19969bb93d97e3e0fa475a
Smrutiranjan <smrutiranjan1582@gmail.com>
2026-08-22 21:44:03 +0530

    704. Binary Search

---
+++ Array/LeetCode705.java
@@ -0,0 +1,33 @@
+//704. Binary Search
+
+// Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+// Example 1:
+
+// Input: nums = [-1,0,3,5,9,12], target = 9
+// Output: 4
+// Explanation: 9 exists in nums and its index is 4
+
+class Solution {
+    public int search(int[] nums, int target) {
+        //by using binary search
+        
+        int low =0,high=nums.length-1;
+        while(low<=high){
+            int mid = low + (high - low) / 2;;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return -1;
+    }
+}
